95e6e57461c8efdf1d8b14dad87b231855e1a3e5
muhammad-sayem <shahrulislamsayem@gmail.com>
2026-09-05 19:42:21 +0600

    Anonymous callback function

--- 20. Callback Function/main.go
+++ 20. Callback Function/main.go
@@ -6,7 +6,7 @@ import "fmt"
 // 	greet()
 // }
 
-func calculate (a int, b int, operation func (x int, y int) int) int {
+func calculate(a int, b int, operation func(x int, y int) int) int {
 	result := operation(a, b)
 	return result
 }
@@ -18,18 +18,30 @@ func main() {
 	// process(sayHello)
 
 	add := func(a int, b int) int {
-		return a+b
+		return a + b
 	}
 
-	sub := func (a int, b int) int {
-		return a-b
+	sub := func(a int, b int) int {
+		return a - b
 	}
 
-	multiply := func (a int, b int) int {
-		return a*b
+	multiply := func(a int, b int) int {
+		return a * b
 	}
 
 	fmt.Println(calculate(30, 10, add))
 	fmt.Println(calculate(50, 40, sub))
 	fmt.Println(calculate(10, 2, multiply))
+
+	//* Annonymus Callback Function *//
+
+	result1 := calculate(60, 30, func(x int, y int) int {
+		return x + y
+	})
+	fmt.Println("Summation:", result1)
+	
+	result2 := calculate(50, 10, func(x int, y int) int {
+		return x - y
+	})
+	fmt.Println("Subtraction:", result2)
 }
